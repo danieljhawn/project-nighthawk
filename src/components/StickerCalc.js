019e83c229f4365371d47ae6b5d3165c6bb66801
Daniel Hawn <danieljhawn@gmail.com>
2020-03-04 15:10:35 -0600

    changes the base rates to match actual rates

--- src/components/StickerCalc.js
+++ src/components/StickerCalc.js
@@ -6,7 +6,7 @@ function StickerCalc() {
     const [width, setWidth] = useState(0);
     const [height, setHeight] = useState(0);
     const [qty, setQty] = useState(0);
-  
+
 
     const handleInputChange = event => {
         const { name, value } = event.target
@@ -26,62 +26,61 @@ function StickerCalc() {
 
     function resetBase(totalArea) {
         switch (true) {
-            case (totalArea < 100):
-                return 0.1;
-            case (totalArea > 100):
-                return 0.05;
+            case (totalArea <= 2500):
+                return 0.0457;
+            case (totalArea > 2500):
+                return 0.0402;
             default:
-                return 69;
+                return 0;
         }
     };
 
 
-    const area =  width * height;
+    const area = width * height;
     const totalArea = area * qty;
     const base = resetBase(totalArea);
     const cost = totalArea * base;
 
-    return (
-        <div className="p3">
-            
-            <div>Width</div>
-            <Form className="form">
-                <input
-                    value={width}
-                    name="width"
-                    onChange={handleInputChange}
-                    type="number"
-                    placeholder="0.0"
-                /><br /><br />
-                <div>height</div>
-                <input
-                    value={height}
-                    name="height"
-                    onChange={handleInputChange}
-                    type="number"
-                    placeholder="0.0"
-                /><br /><br />
-                <div>Quantity</div>
-                <input
-                    value={qty}
-                    name="qty"
-                    onChange={handleInputChange}
-                    type="number"
-                    placeholder="100"
-                /><br /><br />
-                <div>Base Price - ${base}</div>
-                <div>Total Cost - ${cost}</div>
-                <div>Sticker area - { area }</div>
-                <div>Total area - {totalArea}</div>
+    return ( <
+        div className = "p3" >
 
-                {console.log("area - " + [area])}
-                {console.log("qty - " + [qty])}
-                {console.log("base - $" + [base])}
-                {console.log("total area - " + [totalArea])}
-                {console.log("cost - $" + [cost])}
-            </Form>
+        <
+        div > Width < /div> <
+        Form className = "form" >
+        <
+        input value = { width }
+        name = "width"
+        onChange = { handleInputChange }
+        type = "number"
+        placeholder = "0.0" /
+        > < br / > < br / >
+        <
+        div > height < /div> <
+        input value = { height }
+        name = "height"
+        onChange = { handleInputChange }
+        type = "number"
+        placeholder = "0.0" /
+        > < br / > < br / >
+        <
+        div > Quantity < /div> <
+        input value = { qty }
+        name = "qty"
+        onChange = { handleInputChange }
+        type = "number"
+        placeholder = "100" /
+        > < br / > < br / >
+        <
+        div > Base Price - $ { base } < /div> <
+        div > Total Cost - $ { cost } < /div> <
+        div > Sticker area - { area } < /div> <
+        div > Total area - { totalArea } < /div>
 
-        </div>
+        { console.log("area - " + [area]) } { console.log("qty - " + [qty]) } { console.log("base - $" + [base]) } { console.log("total area - " + [totalArea]) } { console.log("cost - $" + [cost]) } <
+        /Form>
+
+        <
+        /div>
     )
 }
 
