c19ab4053b9f1f6a71d7c84dde08bfb9099efc0d
Daniel Hawn <51691090+danieljhawn@users.noreply.github.com>
2020-03-05 22:58:16 -0600

    Update StickerCalc.js
    finished stickerCalc

--- src/components/StickerCalc.js
+++ src/components/StickerCalc.js
@@ -6,7 +6,7 @@ function StickerCalc() {
     const [width, setWidth] = useState(0);
     const [height, setHeight] = useState(0);
     const [qty, setQty] = useState(0);
-    const [shape, setShape] = useState("Select Shape");
+    const [shape, setShape] = useState("Please Select A Shape");
 
 
     const handleInputChange = event => {
@@ -21,120 +21,111 @@ function StickerCalc() {
             case "qty":
                 setQty(value)
                 break
-            case "Rectangle":
-                setShape(value)
-                break
-            case "Simple Shape":
-                setShape(value)
-                break
-            case "Custom Cut":
+            case "shape":
                 setShape(value)
                 break
             default:
         }
     }
 
-
-
     function resetBase(totalArea) {
         switch (true) {
             case (totalArea < 2500):
-                console.log("0.0381")
                 return 0.0381;
             case (totalArea < 5000):
-                console.log("0.0335")
                 return 0.0335;
             case (totalArea < 7500):
-                console.log("0.0231")
                 return 0.0231;
             case (totalArea < 10000):
-                console.log("0.0208")
                 return 0.0208;
             case (totalArea < 15000):
-                console.log("0.0196")
                 return 0.0196;
             case (totalArea < 18000):
-                console.log("0.0185")
                 return 0.0185;
             case (totalArea < 21000):
-                console.log("0.0173")
                 return 0.0173;
             case (totalArea <= 25000):
-                console.log("0.0168")
                 return 0.0168;
             default:
                 return "Please email us your artwork for a quote.";
         }
     };
 
+    function shapeMulti(cost) {
+        switch (true) {
+            case (shape === "Rectangle"):
+                return 1;
+            case (shape === "Simple Shape"):
+                return 1.2;
+            case (shape === "Custom Cut"):
+                return 1.4;
+            default:
+                return 0;
+        }
+    };
 
     const area = width * height;
     const totalArea = area * qty;
     const base = resetBase(totalArea);
     const cost = totalArea * base;
+    const totalCost = cost * shapeMulti(cost);
 
-    return ( <
-        div className = "p-3 bg-light col-6 mx-auto" >
+    return (
+        <div className="row">
+            <div className="p-3 m-3 bg-light col-6 mx-auto rounded-lg shadow-lg">
+                <div> Width </div>
+                <div className="">
+                    <Form className="form">
+                        <input value={width}
+                            className="shadow rounded"
+                            name="width"
+                            onChange={handleInputChange}
+                            type="number"
+                            placeholder="0.0" />
+                        <br /> <br />
+                        <div>Height</div>
+                        <input value={height}
+                            className="shadow rounded"
+                            name="height"
+                            onChange={handleInputChange}
+                            type="number"
+                            placeholder="0.0" />
+                        <br /> <br />
 
-        <
-        div > Width < /div> <
-        Form className = "form" >
-        <
-        input value = { width }
-        name = "width"
-        onChange = { handleInputChange }
-        type = "number"
-        placeholder = "0.0" /
-        >
-        <
-        br / > < br / >
-        <
-        div > height < /div> <
-        input value = { height }
-        name = "height"
-        onChange = { handleInputChange }
-        type = "number"
-        placeholder = "0.0" /
-        >
-        <
-        br / > < br / >
+                        <select name="shape" onChange={handleInputChange} className="shadow rounded">
+                            <option value={shape} >Select Shape</option>
+                            <option name="Rectangle" >Rectangle</option>
+                            <option name="Simple Shape" >Simple Shape</option>
+                            <option name="Custom Cut" >Custom Cut</option>
+                        </select><br></br><br></br>
 
-        <
-        select >
-        <
-        option value = { shape }
-        onChange = { handleInputChange } > Select Shape < /option> <
-        option value = "rectangle" > Rectangle < /option> <
-        option value = "Simple Shape" > Simple Shape < /option> <
-        option value = "Custom Cut" > Custom Cut < /option> < /
-        select >
+                        <div> Quantity </div>
+                        <input value={qty}
+                            className="shadow rounded-lg"
+                            name="qty"
+                            onChange={handleInputChange}
+                            type="number"
+                            placeholder="100" />
+                        <br /> <br />
 
-        <
-        div > Quantity < /div> <
-        input value = { qty }
-        name = "qty"
-        onChange = { handleInputChange }
-        type = "number"
-        placeholder = "100" /
-        >
-        <
-        br / > < br / >
+                        <input type="file" className="shadow rounded" />< br />< br />
 
-        <
-        input type = "file" / > < br / > < br / >
+                        <div> Shape - {shape}</div>
+                        <div> Sticker Area - {area}</div>
+                        <div> Total Area - {totalArea}</div>
+                        <div><strong> Total Cost - $ {totalCost} </strong></div>
 
-        <
-        div > Base Price - $ { base } < /div> <
-        div > Total Cost - $ { cost } < /div> <
-        div > Sticker area - { area } < /div> <
-        div > Total area - { totalArea } < /div> <
-        div > Shape - { shape } < /div>
-
-        { console.log("area - " + [area]) } { console.log("qty - " + [qty]) } { console.log("base - $" + [base]) } { console.log("total area - " + [totalArea]) } { console.log("cost - $" + [cost]) } <
-        /Form>
-
-        <
-        /div>
+                        {console.log("area - " + [area])}
+                        {console.log("qty - " + [qty])}
+                        {console.log("base - $" + [base])}
+                        {console.log("total area - " + [totalArea])}
+                        {console.log("cost - $" + [cost])}
+                        {console.log("tota cost - $" + [totalCost])}
+                        {console.log("shape - " + [shape])}
+                    </Form>
+                </div>
+            </div>
+        </div>
     )
 }
 
